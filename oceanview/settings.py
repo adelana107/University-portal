339ea185cba2f58ef11c813a82cb23009e068eee
Adelana107 <adelana787898@gmail.com>
2025-03-19 15:59:39 +0100

    i added my css to the frontend

--- oceanview/settings.py
+++ oceanview/settings.py
@@ -112,8 +112,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "crm/static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
